0145ae6cc9b795e1ecb5251f95b432a0ec3a4b28
boxy8 <zou.emily@yahoo.co.nz>
2023-08-16 17:58:51 +1200

    feat: add getters and setters for event fields

--- app/src/main/java/com/example/a310_rondayview/Event.java
+++ app/src/main/java/com/example/a310_rondayview/Event.java
@@ -1,15 +1,71 @@
 package com.example.a310_rondayview;
 
+import com.google.firebase.firestore.DocumentId;
+
 import java.util.Date;
 
 public class Event {
-    String eventID;
-    String title;
-    String description;
-    String location;
-    Date time;
-    void updateDetails(){}
-    void delete(){}
+    @DocumentId
+    private String documentId;
+    private String title;
+    private String description;
+    private String location;
+    private Date time;
+
+    public Event() {}
+    public Event(String title, String description, String location, Date time) {
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.time = time;
+    }
+
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    void updateDetails(){
+        // TODO
+    }
+    void delete(){
+        // TODO
+    }
 
 
 }
